91cbb7d57dbb57fe129357c4dcb8ba76fdd1c91b
Sayanmahi <sayanmukherjeedec812@gmail.com>
2023-08-03 20:38:26 +0530

    Added cart icon in navbar where no. of items in cart is displayed

---
+++ ETicket/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,20 @@
+﻿using ETicket.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicket.Data.ViewComponents
+{
+    
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly ShoppingCart shoppingCart;
+        public ShoppingCartSummary(ShoppingCart _shoppingCart)
+        {
+            shoppingCart = _shoppingCart;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var item = shoppingCart.GetShoppingCartItems();
+            return View(item.Count);
+        }
+    }
+}
