d7dd979f4182c962b48be56dbf5bff136c54d264
Sayanmahi <sayanmukherjeedec812@gmail.com>
2023-08-04 21:13:41 +0530

    addes user classes useing identityuser framewrok,configured user roles and added 2 users hard coded

---
+++ ETicket/Data/AppDbInitializer.cs
@@ -0,0 +1,54 @@
+﻿using ETicket.Data.Static;
+using ETicket.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity.Client;
+
+namespace ETicket.Data
+{
+    public class AppDbInitializer
+    {
+
+            public static async Task SeedUserAndRolesAsync(IApplicationBuilder applicationBuilder)
+            {
+                using(var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+                {
+                   var roleManager=serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+
+                if (!await roleManager.RoleExistsAsync(UserRoles.User))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+                var UserManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                var adminUser=await UserManager.FindByEmailAsync("admin@etickets.com");
+                if(adminUser==null)
+                {
+                    var newAdminUser = new ApplicationUser()
+                    {
+                        FullName = "Admin User",
+                        UserName = "admin-user",
+                        Email = "admin@etickets.com",
+                        EmailConfirmed = true
+                    };
+                    await UserManager.CreateAsync(newAdminUser,"Coding@1234?");
+                    await UserManager.AddToRoleAsync(newAdminUser, UserRoles.Admin);
+                }
+
+                var appUser = await UserManager.FindByEmailAsync("user@etickets.com");
+                if (appUser == null)
+                {
+                    var newAppUser = new ApplicationUser()
+                    {
+                        FullName = "Application User",
+                        UserName = "app-user",
+                        Email = "user@etickets.com",
+                        EmailConfirmed = true
+                    };
+                    await UserManager.CreateAsync(newAppUser, "Coding@1234?");
+                    await UserManager.AddToRoleAsync(newAppUser, UserRoles.Admin);
+                }
+
+
+            }
+        }
+    }
+}
--- ETicket/Program.cs
+++ ETicket/Program.cs
@@ -1,7 +1,10 @@
 using ETicket.Data;
 using ETicket.Data.Cart;
 using ETicket.Data.Services;
+using ETicket.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,7 +18,14 @@ builder.Services.AddScoped<IMovieService, MovieService>();
 builder.Services.AddScoped<IOrdersService, OrdersService>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
 builder.Services.AddSession();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme=CookieAuthenticationDefaults.AuthenticationScheme;
+
+});
 
 
 var app = builder.Build();
@@ -33,10 +43,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 app.UseSession();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+AppDbInitializer.SeedUserAndRolesAsync(app).Wait();
 app.Run();
