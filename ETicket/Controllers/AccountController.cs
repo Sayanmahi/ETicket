c47614b3eb63327db9c5d040cd2e8aee6f8fc489
Sayanmahi <sayanmukherjeedec812@gmail.com>
2023-08-04 21:39:25 +0530

    Login view and account controller login action created

---
+++ ETicket/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using ETicket.Data;
+using ETicket.Data.ViewModel;
+using ETicket.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicket.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly AppDbContext appDbContext;
+        public AccountController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager, AppDbContext _appDbContext)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+            appDbContext = _appDbContext;
+            
+        }
+        public IActionResult Login()
+        {
+            var response = new LoginVM();
+            return View(response);
+        }
+    }
+}
---
+++ ETicket/Data/ViewModel/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ETicket.Data.ViewModel
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage ="Email Address is required")]
+        [Display(Name ="Email Address")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
